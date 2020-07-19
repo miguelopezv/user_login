021e7d59384f8c778322f343f9849697edfcf6aa
miguelopezv <miguelopezv@gmail.com>
2020-07-18 23:24:11 -0500

    show data in home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../bloc/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Email: ${bloc.email}'),
+          Divider(),
+          Text('Password: ${bloc.password}')
+        ],
+      ),
+    );
+  }
+}
